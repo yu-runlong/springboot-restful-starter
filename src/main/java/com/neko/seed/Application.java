39700e9cd9654d932fbb9c84da16fd5d5e194231
YuRunLong <1260230836@qq.com>
2022-10-16 16:06:13 +0800

    主启动类还原

--- src/main/java/com/neko/seed/Application.java
+++ src/main/java/com/neko/seed/Application.java
@@ -3,9 +3,6 @@ package com.neko.seed;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * 主启动类
- */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
