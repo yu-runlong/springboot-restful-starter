06b1c52ef3008c20c0cd5176882f188d2ec7bd1a
YuRunLong <1260230836@qq.com>
2022-10-16 16:06:55 +0800

    1.1

--- src/main/java/com/neko/seed/Application.java
+++ src/main/java/com/neko/seed/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        System.out.println("项目启动中...");
         SpringApplication.run(Application.class, args);
     }
 }
